2b1d6f19e8f44bf0e17c296b9af901ec55d8ad6e
Ryne Stajcar <rynostajcar@gmail.com>
2018-02-19 17:21:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,7 @@ class Scraper
     doc = Nokogiri::HTML(open(profile_url))
     media_links = {}
     doc.css("div.social-icon-container").each do |link|
+      binding.pry
       twitter = link.css("a")[0].values[0]
       linkedin = link.css("a")[1].values[0]
       github = link.css("a")[2].values[0]
