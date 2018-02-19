0e4a1d846ddb9bfe66567dfde364dadf90acc18b
Ryne Stajcar <rynostajcar@gmail.com>
2018-02-19 16:11:31 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,13 +23,13 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = File.read(profile_url)
     doc = Nokogiri::HTML(html)
-    media_links = []
+    media_links = {}
     doc.css("div.social-icon-container").each do |link|
       twitter = link.css("a")[0].values[0]
       linkedin = link.css("a")[1].values[0]
       github = link.css("a")[2].values[0]
       blog = link.css("a")[3].values[0]
-      media_links << {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog}
+      media_links = {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog}
     end
     #binding.pry
     media_links
